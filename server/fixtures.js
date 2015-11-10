f2fd1ea2c7c42cc4ec6c17093b617868e02f00f0
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-11-10 12:47:58 +0100

    Better quoting

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,7 @@
 if (Tweets.find().count() === 0) {
-  Tweets.insert({
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  });
+  Tweets.insert({"created_at":"Tue Nov 03 14:13:42 +0000 2015","id":661546799761629184,"id_str":"661546799761629184","text":".@TrayJoinzz Welcome to Puffalo  https:\/\/t.co\/cYDDu6qbUQ", "screen_name": "bigbizzinc"});
 
-  Tweets.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  });
+  Tweets.insert({"created_at":"Tue Nov 03 14:13:42 +0000 2015","id":661546799749046274,"id_str":"661546799749046274","text":"Man Cave Ideas is out! https:\/\/t.co\/ps0lPFprWQ Stories via @PenthouseQueen @GarageOrganiz @JonesGlassDeco"});
 
-  Tweets.insert({
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  });
+  Tweets.insert({"created_at":"Tue Nov 03 14:13:42 +0000 2015","id":661546799753240576,"id_str":"661546799753240576","text":"@spinney (If so, good job!)"});
 }
