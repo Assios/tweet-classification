a6a53de9aca36a92a3e6102be8dd544e69e93b77
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-11-10 23:27:41 +0100

    Refactoring

--- client/templates/tweet.js
+++ client/templates/tweet.js
@@ -1,7 +1,44 @@
-Template.tweet.helpers({
-    getRandomTweet: function() {
-        random = Math.floor(Math.random() * 2) + 1;
+Template.tweet.events({
 
-        return Tweets.find({"index": random});
-    }
+  "click .positive": function () {
+
+    console.log("voted positive");
+
+    Tweets.update(this._id, {
+
+      $set: {sentiment: "positive"}
+
+    });
+
+    Router.go('tweet', {_id: this.index + 1});
+
+  },
+
+  "click .neutral": function () {
+
+    console.log("voted neutral");
+
+    Tweets.update(this._id, {
+
+      $set: {sentiment: "neutral"}
+
+    });
+
+    Router.go('tweet', {_id: this.index + 1});
+
+  },
+
+  "click .negative": function () {
+
+    console.log("voted negative");
+
+    Tweets.update(this._id, {
+
+      $set: {sentiment: "negative"}
+
+    });
+
+    Router.go('tweet', {_id: this.index + 1});
+
+  }
 });
